3779da061e6d642b7cd8ccae79ecb9b1efcead0b
Kevin Le <kevinle108@gmail.com>
2021-11-22 13:58:23 -0500

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<main>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sunt magni illum molestias similique quos mollitia ratione accusamus. Ab officia cumque eius nemo fugit repudiandae adipisci voluptate exercitationem maxime ea.</p>
+</main>
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
