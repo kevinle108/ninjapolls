2326d0eae7706447a327ad26543b94e38156a782
Kevin Le <kevinle108@gmail.com>
2021-12-05 16:41:29 -0500

    add PollDetails

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,6 +1,8 @@
 <script>
   import Button from "../shared/Button.svelte";
+  import { createEventDispatcher } from "svelte";
 
+  let dispatch = createEventDispatcher();
   let fields = {
     question: '',
     answerA: '',
@@ -43,6 +45,8 @@
 
     if (valid) {
       console.log('valid', fields);
+      let poll = {...fields, votesA: 0, votesB: 0};
+      dispatch('add', poll)
     }    
   }
 
---
+++ src/components/PollList.svelte
@@ -0,0 +1,20 @@
+<script>
+  import PollDetails from "./PollDetails.svelte";
+  export let polls = [];
+
+</script>
+
+<div class="poll-list">
+  {#each polls as poll (poll.id)}
+    <PollDetails {poll}/>
+  {/each}
+</div>
+
+<style>
+  .poll-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+  }
+
+</style>
